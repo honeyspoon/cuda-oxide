5d32bca99373733fbe7e2e31a3b86d36d590bc86
abder <bobmatt911@gmail.com>
2026-07-24 12:35:28 -0400

    feat: add common non-power-of-2 unroll factors (5,6,7,10,12,24,64)
    Signed-off-by: abder <bobmatt911@gmail.com>

--- crates/cuda-device/src/unroll.rs
+++ crates/cuda-device/src/unroll.rs
@@ -114,6 +114,80 @@ macro_rules! unroll {
         { const $i: usize = 30; $body }
         { const $i: usize = 31; $body }
     };
+    // Non-power-of-2 range forms: delegate to nearest power-of-2 + remainder
+    ($i:ident in 0..5 => $body:block) => {
+        $crate::unroll!($i in 0..4 => $body);
+        { const $i: usize = 4; $body }
+    };
+    ($i:ident in 0..6 => $body:block) => {
+        $crate::unroll!($i in 0..4 => $body);
+        { const $i: usize = 4; $body }
+        { const $i: usize = 5; $body }
+    };
+    ($i:ident in 0..7 => $body:block) => {
+        $crate::unroll!($i in 0..4 => $body);
+        { const $i: usize = 4; $body }
+        { const $i: usize = 5; $body }
+        { const $i: usize = 6; $body }
+    };
+    ($i:ident in 0..10 => $body:block) => {
+        $crate::unroll!($i in 0..8 => $body);
+        { const $i: usize = 8; $body }
+        { const $i: usize = 9; $body }
+    };
+    ($i:ident in 0..12 => $body:block) => {
+        $crate::unroll!($i in 0..8 => $body);
+        { const $i: usize = 8; $body }
+        { const $i: usize = 9; $body }
+        { const $i: usize = 10; $body }
+        { const $i: usize = 11; $body }
+    };
+    ($i:ident in 0..24 => $body:block) => {
+        $crate::unroll!($i in 0..16 => $body);
+        { const $i: usize = 16; $body }
+        { const $i: usize = 17; $body }
+        { const $i: usize = 18; $body }
+        { const $i: usize = 19; $body }
+        { const $i: usize = 20; $body }
+        { const $i: usize = 21; $body }
+        { const $i: usize = 22; $body }
+        { const $i: usize = 23; $body }
+    };
+    ($i:ident in 0..64 => $body:block) => {
+        $crate::unroll!($i in 0..32 => $body);
+        { const $i: usize = 32; $body }
+        { const $i: usize = 33; $body }
+        { const $i: usize = 34; $body }
+        { const $i: usize = 35; $body }
+        { const $i: usize = 36; $body }
+        { const $i: usize = 37; $body }
+        { const $i: usize = 38; $body }
+        { const $i: usize = 39; $body }
+        { const $i: usize = 40; $body }
+        { const $i: usize = 41; $body }
+        { const $i: usize = 42; $body }
+        { const $i: usize = 43; $body }
+        { const $i: usize = 44; $body }
+        { const $i: usize = 45; $body }
+        { const $i: usize = 46; $body }
+        { const $i: usize = 47; $body }
+        { const $i: usize = 48; $body }
+        { const $i: usize = 49; $body }
+        { const $i: usize = 50; $body }
+        { const $i: usize = 51; $body }
+        { const $i: usize = 52; $body }
+        { const $i: usize = 53; $body }
+        { const $i: usize = 54; $body }
+        { const $i: usize = 55; $body }
+        { const $i: usize = 56; $body }
+        { const $i: usize = 57; $body }
+        { const $i: usize = 58; $body }
+        { const $i: usize = 59; $body }
+        { const $i: usize = 60; $body }
+        { const $i: usize = 61; $body }
+        { const $i: usize = 62; $body }
+        { const $i: usize = 63; $body }
+    };
     // List form: unroll!(i in [a, b, c] => { body })
     ($i:ident in [$($val:expr),+ $(,)?] => $body:block) => {
         $( { const $i: usize = $val; $body } )+
